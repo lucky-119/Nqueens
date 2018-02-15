19de604373929048afde013f9579cf541039a478
lucky-119 <35452292+lucky-119@users.noreply.github.com>
2018-02-15 11:39:34 +0530

    Nqueens_Commit2

--- Nqueens.py
+++ Nqueens.py
@@ -1,15 +1,13 @@
 import numpy as np
 
 count=0
-def place(i,j):
-    a[i,j]='1'
 
-def bound(x,y):
+def bound(x,y):                                             #To check if (x,y) lies within matrix a
     if(x<0 or y<0 or x>n-1 or y>n-1):
         return False
     return True
 
-def check(x,y):
+def check(x,y):                                             #To check whether a queen can be placed at (x,y)
     for i in range(0,n):
         if(a[x,i]==1 or a[i,y]==1):
             return False
@@ -27,17 +25,21 @@ def check(x,y):
                 return False
     return True
 
-def compare(x,y):
+def compare(x,y):                                           #To find the positions in a matrix where a queen can be placed
     global count
     for i in range(0,n):
         for j in range(0,n):
             if(check(i,j)==True and (i!=x or j!=y)):
-                place(i,j)
-                p.append(i)
-                q.append(j)
+                a[i,j]=1
+                if(x==-1):
+                    p.append(i)
+                    q.append(j)
+                else:
+                    p.insert(0,i)
+                    q.insert(0,j)
                 count=count+1
                 
-def backtrack(x,y):
+def backtrack(x,y):                                         #To backtrack to find other combinations so that there are n queens placed 
     global count
     a[x,y]=0
     count=count-1
@@ -52,20 +54,4 @@ while(count<n):
      backtrack(p.pop(),q.pop())
 print a
 
-    
-
-        
-        
-            
-        
-            
-    
-    
-
-
-
-
-
-                                            #Create n*n array
-                                            #Place a queen in a corner
                                             
